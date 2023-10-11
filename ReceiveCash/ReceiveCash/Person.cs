6c9ccaa3abdd5f22ac1dd19a6d93e2bbc4208b09
Dmytro Kulinich <spo1091@i.ua>
2023-10-11 12:59:37 +0300

    Add proj

---
+++ ReceiveCash/ReceiveCash/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveCash
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Cash { get; set; }
+
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine("Я " +  Name + ". Я маю - " + Cash + "гривень.");
+        }
+
+        public int GiveCash(int cash)
+        {
+            int myCash = Cash;
+            if (cash < 0)
+            {
+                Console.WriteLine("Ти ввів суму меньшу за 0!");
+                return 0;
+            }
+            if (myCash < cash)
+            {
+                Console.WriteLine("В мене немає сітльки грошей.");
+                return 0;
+            }
+            else
+            {
+                Cash = myCash - cash;
+                return cash;
+            }
+        }
+
+        public void ReceiveCash(int cash)
+        {
+            int myCash = Cash;
+            if (cash < 0)
+            {
+                return;
+            }
+            else
+            {
+                Cash = myCash + cash;
+            }
+        }
+    }
+}
---
+++ ReceiveCash/ReceiveCash/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReceiveCash;
+
+Person person1 = new Person { Name = "Ваня", Cash = 100 };
+Person person2 = new Person { Name = "Саша", Cash = 100 };
+
+while (true)
+{
+    person1.WriteMyInfo();
+    person2.WriteMyInfo();
+
+    Console.Write("Введи кількість: ");
+    string howMutch = Console.ReadLine();
+    if (howMutch == " ") return;
+    if (int.TryParse(howMutch, out int result))
+    {
+        int cash = int.Parse(howMutch);
+        Console.Write("Хто повинен дати кошти: ");
+        string witchPerson = Console.ReadLine();
+        if(witchPerson == "Ваня")
+        {
+            person2.ReceiveCash(person1.GiveCash(cash));
+        }
+        else if (witchPerson == "Саша")
+        {
+            person1.ReceiveCash(person2.GiveCash(cash));
+        }
+        else
+        {
+            Console.WriteLine("Напише це Саша чи Ваня");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Веди кількість грошей або пробіл  для виходу.");
+    }
+}
